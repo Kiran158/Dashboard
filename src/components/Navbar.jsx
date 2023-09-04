a52b00bf23ea652b7b28fb5bbc2b6b41de263643
kiran <kbb1582001@gmail.com>
2023-09-04 09:32:18 +0530

    Make changes and fix the bug

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -77,7 +77,6 @@ const Navbar = () => {
             <MdKeyboardArrowDown className="text-gray-400 text-14" />
           </div>
         </TooltipComponent>
-
         {isClicked.cart && (<Cart />)}
         {isClicked.chat && (<Chat />)}
         {isClicked.notification && (<Notification />)}
